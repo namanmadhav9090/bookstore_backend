09352299a3af842c38978002dfb9def41936f50f
namanmadhav9090 <118351229+namanmadhav9090@users.noreply.github.com>
2024-04-07 10:46:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const cartRoutes = require('./routes/cart');
   
 app.use(cors(
   {
-    origin: [https://deploy-mern-1whq.vercel.app],
+    origin: ["https://deploy-mern-1whq.vercel.app"],
     method: ["POST","GET","PUT","DELETE"],
     credentials: true
   }
