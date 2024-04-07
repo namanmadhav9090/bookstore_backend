bb71fe6137c33dd0a00b22a8559c9a5fa38cd64f
namanmadhav9090 <118351229+namanmadhav9090@users.noreply.github.com>
2024-04-07 11:41:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get("/",(req, res)=>{
-    return res.send("Ok");
+    return res.status(200).send("Hello vercel");
 })
 // Routes
 app.use('/api/books', bookRoutes);
